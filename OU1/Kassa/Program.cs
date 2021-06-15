8c0840e8ecaccf8bee254e15790974315caa4542
myllern <49235951+myllern@users.noreply.github.com>
2021-06-16 00:20:17 +0200

    Små fix kvar

---
+++ OU1/Kassa/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace OU1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+//TODO ändra texterna 
+                Console.WriteLine("Ange pris :");
+                int price = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("ange hur mycket du har betalat :");
+                int deposit = int.Parse(Console.ReadLine());
+                
+                int sum = deposit - price;
+
+                sum = price - deposit;
+                if (sum > 0)
+                {
+                    Console.WriteLine($"Växel tillbaka: {sum}"
+                    );
+                    Console.WriteLine(nrOfBills(500, sum));
+                    Console.WriteLine(nrOfBills(100, sum));
+                    Console.WriteLine(nrOfBills(50, sum));
+                    Console.WriteLine(nrOfBills(20, sum));
+                    Console.WriteLine(nrOfBills(10, sum));
+                    Console.WriteLine(nrOfBills(5, sum));
+                    Console.WriteLine(nrOfBills(1, sum));
+
+                }
+
+                if (sum == 0)
+                {
+                    Console.WriteLine("Allt betalt");
+                }
+                else
+                {
+                    sum = deposit - price;
+                    Console.WriteLine("Ej tillräcklig betalning... ");
+                }
+
+
+                //TODO: Fixa sum så den minskaar eller nått
+                static String nrOfBills(int val, int sum)
+                {
+                    String s = $"{(sum - sum % val) / val} lappar:";
+                    sum = sum % val;
+                    return s;
+                }
+            }
+        }
+    }
+
